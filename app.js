77e78d47c34a6937b88968b345ee0c16e8b0c7c2
juanpab1o <dev@i-tecnologia.co>
2021-07-15 20:23:12 -0500

    Ajustes varios

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+const dotenv = require('dotenv').config({ path: '.env' })
 const express = require('express'), mysql = require('mysql'), helmet = require('helmet'),
-    bodyParser = require('body-Parser'), PORT = process.env.PORT || 3010, app = express();
+    bodyParser = require('body-Parser'), PORT = process.env.PORT, app = express();
 
 
 app.use(bodyParser.json());
@@ -9,19 +10,44 @@ app.use(helmet());
 
 //Mysql
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'krypton'
+    host: process.env.PS_DB_HOST,
+    user: process.env.PS_DB_USER,
+    password: process.env.PS_DB_PASSWORD,
+    database: process.env.PS_DB_DATABASE
 });
 //Routes
 app.get('/', (req, res) => {
-    res.send('API CONNECT');
+    res.sendFile('./status.html', { root: __dirname });
 });
 
 // Products
 app.get('/products', (req, res)=>{
-   const sql = 'SELECT * FROM ps_product';
+   const sql = 'SELECT SQL_CALC_FOUND_ROWS p.`id_product`  AS `id_product`,\n' +
+       ' p.`reference`  AS `referencia`,\n' +
+       ' sa.`price`  AS `precio`,\n' +
+       ' p.`id_shop_default`  AS `id_shop_default`,\n' +
+       ' p.`is_virtual`  AS `virtual`,\n' +
+       ' pl.`name`  AS `nombre`,\n' +
+       ' pl.`link_rewrite`  AS `link_rewrite`,\n' +
+       ' sa.`active`  AS `activo`,\n' +
+       ' shop.`name`  AS `shopname`,\n' +
+       ' image_shop.`id_image`  AS `id_image`,\n' +
+       ' cl.`name`  AS `categoria`,\n' +
+       ' 0 AS `precio_final`,\n' +
+       ' pd.`nb_downloadable`  AS `nb_downloadable`,\n' +
+       ' sav.`quantity`  AS `sav_quantity`,\n' +
+       ' IF(sav.`quantity`<=0, 1, 0) AS `badge_danger` \n' +
+       'FROM  `ps_product` p \n' +
+       ' LEFT JOIN `ps_product_lang` pl ON (pl.`id_product` = p.`id_product` AND pl.`id_lang` = 1 AND pl.`id_shop` = 1) \n' +
+       ' LEFT JOIN `ps_stock_available` sav ON (sav.`id_product` = p.`id_product` AND sav.`id_product_attribute` = 0 AND sav.id_shop = 1  AND sav.id_shop_group = 0 ) \n' +
+       ' JOIN `ps_product_shop` sa ON (p.`id_product` = sa.`id_product` AND sa.id_shop = 1) \n' +
+       ' LEFT JOIN `ps_category_lang` cl ON (sa.`id_category_default` = cl.`id_category` AND cl.`id_lang` = 1 AND cl.id_shop = 1) \n' +
+       ' LEFT JOIN `ps_category` c ON (c.`id_category` = cl.`id_category`) \n' +
+       ' LEFT JOIN `ps_shop` shop ON (shop.id_shop = 1) \n' +
+       ' LEFT JOIN `ps_image_shop` image_shop ON (image_shop.`id_product` = p.`id_product` AND image_shop.`cover` = 1 AND image_shop.id_shop = 1) \n' +
+       ' LEFT JOIN `ps_image` i ON (i.`id_image` = image_shop.`id_image`) \n' +
+       ' LEFT JOIN `ps_product_download` pd ON (pd.`id_product` = p.`id_product`) \n' +
+       'WHERE (1 AND state = 1)';
 
    connection.query(sql, (error, results) =>{
        if(error) throw error;
@@ -34,6 +60,21 @@ app.get('/products', (req, res)=>{
 
 
 });
+
+/*app.get('/products/list', (req, res)=>{
+    const sql = 'SELECT * FROM ps_product';
+
+    connection.query(sql, (error, results) =>{
+        if(error) throw error;
+        if(results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('Not results');
+        }
+    });
+
+
+});*/
 
 app.get('/products/:id', (req, res)=>{
     const {id} = req.params
@@ -82,6 +123,37 @@ app.delete('/delete/:id',(req,res)=>{
         res.send('Product deleted')
     });
 });
+
+app.get('/atr', (req, res)=>{
+    const sql = 'SELECT SQL_CALC_FOUND_ROWS b.*, a.* FROM `ps_attribute_group` a LEFT JOIN `ps_attribute_group_lang` b ON ( b.`id_attribute_group` = a.`id_attribute_group` AND b.`id_lang` = 1 )';
+
+    connection.query(sql, (error, results) =>{
+        if(error) throw error;
+        if(results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('Not results');
+        }
+    });
+
+
+});
+
+app.get('/feat', (req, res)=>{
+    const sql = 'SELECT SQL_CALC_FOUND_ROWS b.*, a.* FROM `ps_feature` a LEFT JOIN `ps_feature_lang` b ON (b.`id_feature` = a.`id_feature` AND b.`id_lang` = 1)';
+
+    connection.query(sql, (error, results) =>{
+        if(error) throw error;
+        if(results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('Not results');
+        }
+    });
+
+
+});
+
 
 //Check connect
 connection.connect(error => {
